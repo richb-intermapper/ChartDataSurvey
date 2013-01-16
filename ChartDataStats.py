229c3f339134f5ab707009c960c5636e5dc810a5
Rich Brown <richb.hanover@gmail.com>
2013-01-15 22:33:46 -0500

    Correlate map names with actual maps in Enabled/Disabled maps into a new column. If not in either, assume map has been deleted.

--- ChartDataStats.py
+++ ChartDataStats.py
@@ -5,11 +5,66 @@ import struct
 import getopt
 
 
-'''
+"""
 Convert seconds (from the epoch) to a desired date format
-'''
+"""
 def toDate(secs):
     return time.strftime("%d%b%Y",time.localtime(secs))
+
+'''
+findChartDir - given the IM settings path, return a path to the Chart Data directory
+'''
+def findChartDir(dir):
+    chartdir = os.path.join(dir, "Chart Data")
+    if (not os.path.exists(chartdir)):
+        chartdir = chartdir + ".noindex"
+    return chartdir
+
+'''
+findMapsDir - return the path the the current IM version's Maps folder (containing Enabled & Disabled folders)
+'''
+def findMapsDir(dir):
+    maps = os.path.join(dir, "Maps")
+    return os.path.join(maps, "5.6")        # stub - needs to find newest #.# directory (ignoring deleted & backup)
+
+'''
+enabledState() - return the customer's map name, as well as whether it's enabled or disabled.
+    If not in either list, assume it's deleted, and return its original mapname/gid.
+'''
+def enabledState(filename, enabledMaps, disabledMaps):
+    mapname = enabledMaps.isInDir(filename)
+    if (mapname != ""):
+        return (mapname, "-")
+    mapname = disabledMaps.isInDir(filename)
+    if (mapname != ""):
+        return (mapname, "Disabled")
+    return (filename, "Deleted")
+
+'''
+mapDir - return the customer's "map name" if the str matches the prefix of one of the file in dir
+    Return "" if it doesn't match the gxxxxxxxx- prefix
+    Used to scan the Maps/Enabled or Maps/Disabeled folders
+'''
+class mapDir():
+
+    def __init__(self, theDir):
+        self.paths = []
+        self.lastmatch = ""
+        self.lastname = ""
+        for root, subFolders, files in os.walk(theDir):
+            for file in files:
+                self.paths.append(file)
+
+    def isInDir(self, gid):
+        if (gid == self.lastmatch):         # if they asked the same question...
+            return self.lastname            # give the same answer
+        for file in self.paths:
+            if (file.find(gid) == 0):       # found the name at first position
+                self.lastmatch = gid
+                self.lastname = file[len(gid)+1:]
+                return self.lastname
+        else:
+            return ""
 
 '''
 Process all the files in the Chart Data folder
@@ -19,7 +74,7 @@ Compute the "age" of the file in number of days since last time stamp
 Ignore "MetaDataCache" files (but count them)
 
 '''
-def ScanChartDataFolder(chartdir, outfile, brief):
+def ScanChartDataFolder(chartdir, mapdir, outfile, brief):
     #================================================================================
     # List of all the files, total count of files and folders & Total size of files.
     # http://mayankjohri.wordpress.com/2008/07/02/create-list-of-files-in-a-dir-tree/
@@ -34,6 +89,8 @@ def ScanChartDataFolder(chartdir, outfile, brief):
     inactivecount = 0
     inactivesize = 0
     newestfile = 0
+    enabledMaps = mapDir(os.path.join(mapdir, "Enabled"))
+    disabledMaps = mapDir(os.path.join(mapdir, "Disabled"))
 
     # sys.path.append(os.path.join(pd, "YOUR-MODULE")) # insert the directory into the sys.path variable
     # rootdir = "/Library/Application Support/InterMapper Settings/Chart Data.noindex"
@@ -56,6 +113,7 @@ def ScanChartDataFolder(chartdir, outfile, brief):
                 if (os.path.getmtime(fpath) > newestfile):
                     newestfile = os.path.getmtime(fpath)    # remember the newest file mod date
 
+
     # print ("Newest: %d, %s") % (newestfile, toDate(newestfile))
 
     for fp in fileList:                                     # scan the files looking for evidence of big/little endian-ness
@@ -73,6 +131,7 @@ def ScanChartDataFolder(chartdir, outfile, brief):
         chtime  = toDate(os.path.getctime(fp))
         modtime = toDate(os.path.getmtime(fp))
         flen = os.path.getsize(fp)
+        (dirname, mapState) = enabledState(dirname, enabledMaps, disabledMaps)
         if (flen <= 0):
             emptyCount += 1
             first = "-"
@@ -102,7 +161,7 @@ def ScanChartDataFolder(chartdir, outfile, brief):
             else:
                 inactive = "-"
             f.close()
-        outstr = dirname + "\t" + filename + "\t" + str(flen) + "\t" + chtime + "\t" + modtime + "\t" + first + "\t" + last + "\t" + inactive
+        outstr = dirname + "\t" + filename + "\t" + mapState  + "\t" + str(flen) + "\t" + chtime + "\t" + modtime + "\t" + first + "\t" + last + "\t" + inactive
 
         fileStats.append(outstr)
 
@@ -115,7 +174,7 @@ def ScanChartDataFolder(chartdir, outfile, brief):
     retstr += "Total Folders: %d\n" % (folderCount)
 
     outfile.write(retstr)
-    outfile.write("Map      \tDatapoint\tLength\tcTime    \tmTime    \tFirst   \tLast    \tDays Idle\n")
+    outfile.write("Map      \tDatapoint\tState\tLength\tcTime    \tmTime    \tFirst   \tLast    \tDays Idle\n")
 
     for line in fileStats:
         outfile.write(line + "\n")
@@ -132,34 +191,34 @@ def main(argv=None):
 
 ### Get the first argument that was passed into the script
     try:
-        opts, args = getopt.getopt(sys.argv[1:], "c:b:", ["charts=", "brief="])
+        opts, args = getopt.getopt(sys.argv[1:], "s:b:", ["settings=", "brief="])
     except getopt.GetoptError, e:
-        print "ChartDataSurvey.p [--charts='directorypath'] [--brief=1]"
+        print "ChartDataSurvey.p [--settings='directorypath'] [--brief=1]"
         sys.exit(1)
 
-    chartdir = ""
+    settingsdir = ""
     brief = False
     for o,a in opts:
-        if o in ("-c", "--charts"):
-            chartdir = check(a)
+        if o in ("-s", "--settings"):
+            settingsdir = check(a)
         elif o in ("-b", "--brief"):
              if (check(a) and a == "1"):
                 brief = True
 
-    wd = os.getcwd()                            # Get path of working directory of the script (InterMapper Settings/Tools/your.domain.your.package)
-    if (chartdir == ""):                        # no chart directory specified
+    if (settingsdir == ""):                     # no chart directory specified, so we're running from Tools directory
+        wd = os.getcwd()                        # Get path of working directory of the script (InterMapper Settings/Tools/your.domain.your.package)
         (toolsd, rem) = os.path.split(wd)       # split off the parent directory - this yields the path to the "Tools" directory
-        (imdir, rem) = os.path.split(toolsd)    # imdir is path to InterMapper Settings directory
-        chartdir = os.path.join(imdir, "Chart Data")
-        if (not os.path.exists(chartdir)):
-            chartdir = chartdir + ".noindex"
-        outfiledir = os.path.join(imdir, "Extensions")
-    else:
-        outfiledir = chartdir           # just put the ChartDataSurvey file in the chart directory
+        (settingsdir, rem) = os.path.split(toolsd)    # settingsdir is path to InterMapper Settings directory
+        outfiledir = os.path.join(settingsdir, "Extensions")
+        chartdir = findChartDir(settingsdir)
+    else:                                       # argument supplied the IM settings directory
+        chartdir = findChartDir(settingsdir)
+        outfiledir = chartdir                   # write the file to the chart directory
 
+    mapdir = findMapsDir(settingsdir)
     datestamp = "ChartDataSurvey-" + toDate(time.time()) + ".txt"
     outfile = open(os.path.join(outfiledir,datestamp), 'w')
-    retstr = ScanChartDataFolder(chartdir, outfile, brief)
+    retstr = ScanChartDataFolder(chartdir, mapdir, outfile, brief)
     outfile.close()
 
     ### Set the return value from the script
