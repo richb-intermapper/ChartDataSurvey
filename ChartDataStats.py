d04ddfe55b19dacfd7e5e5908cef4c560237e27c
Rich Brown <richb.hanover@gmail.com>
2013-01-16 14:51:40 -0500

    Ignore exceptions in toDate()

--- ChartDataStats.py
+++ ChartDataStats.py
@@ -19,8 +19,12 @@ import socket
 def toDate(secs):
     """
     Convert seconds (from the epoch) to a desired date format
+        If time cannot be converted (wrong byte order?) just return "odd-time"
     """
-    return time.strftime("%d%b%Y",time.localtime(secs))
+    try:
+        return time.strftime("%d%b%Y",time.localtime(secs))
+    except:
+        return "odd-time"
 
 def GetMyIPAddr():
     '''
@@ -152,7 +156,7 @@ def ScanChartDataFolder(chartdir, mapdir, outfile, brief, inactivedays):
                 fileList.append(fpath)                      # collect a list of paths of chart data files
                 if (os.path.getmtime(fpath) > newestfile):
                     newestfile = os.path.getmtime(fpath)    # remember the newest file mod date
-    # print ("Newest: %d, %s") % (newestfile, toDate(newestfile))
+        # print ("Newest: %d, %s") % (newestfile, toDate(newestfile))
 
     # Figure out whether the files are big-endian or little-endian
     for fp in fileList:                                     # scan the files looking for evidence of big/little endian-ness
@@ -171,6 +175,7 @@ def ScanChartDataFolder(chartdir, mapdir, outfile, brief, inactivedays):
         chtime  = toDate(os.path.getctime(fp))
         modtime = toDate(os.path.getmtime(fp))
         flen = os.path.getsize(fp)
+        # print "%s %s" % (dirname, filename)
         (dirname, mapState) = enabledState(dirname, enabledMaps, disabledMaps)
         if (mapState == "Deleted"):
             deletedcount += 1
@@ -213,7 +218,7 @@ def ScanChartDataFolder(chartdir, mapdir, outfile, brief, inactivedays):
     retstr += "%d Chart Files containing %d bytes\n" % (len(fileList), fileSize)
     retstr += "%d Chart files containing %d bytes associated with deleted maps \n" % (deletedcount, deletedsize)
     retstr += "%d Inactive files containing %d bytes with no new data for %d days\n" % (inactivecount, inactivesize, inactivedays)
-#    retstr += "Cache files: %d containing %d bytes\n" % (cachecount, cachesize)
+    #    retstr += "Cache files: %d containing %d bytes\n" % (cachecount, cachesize)
     retstr += "%d Empty files\n" % (emptyCount)
     retstr += "%d Total maps\n" % (folderCount)
 
@@ -233,7 +238,7 @@ def main(argv=None):
         else:
             return str(arg) #explicitly type-cast
 
-### Get the first argument that was passed into the script
+        ### Get the first argument that was passed into the script
     try:
         opts, args = getopt.getopt(sys.argv[1:], "s:b:", ["settings=", "brief="])
     except getopt.GetoptError, e:
@@ -246,7 +251,7 @@ def main(argv=None):
         if o in ("-s", "--settings"):
             settingsdir = check(a)
         elif o in ("-b", "--brief"):
-             if (check(a) and a == "1"):
+            if (check(a) and a == "1"):
                 brief = True
 
     if (settingsdir == ""):                     # no chart directory specified, so we're running from Tools directory
@@ -260,7 +265,7 @@ def main(argv=None):
         outfiledir = chartdir                   # write the file to the chart directory
 
     mapdir = findMapsDir(settingsdir)
-    outfilename = "ChartDataSurvey-%s-%s.txt" % (GetMyIPAddr(), toDate(time.time()))
+    outfilename = "ChartDataSurvey-%s.txt" % (toDate(time.time()))
     outfile = open(os.path.join(outfiledir,outfilename), 'w')
     retstr = ScanChartDataFolder(chartdir, mapdir, outfile, brief, 30)
     outfile.close()
@@ -281,7 +286,7 @@ def main(argv=None):
         returnval = 4
     else:
         returnval = 3
-    # severity = "Severity is '%s'; " % arg
+        # severity = "Severity is '%s'; " % arg
 
     ### Print a line to stdout with variables
     print "\{ $val1 := '%s' }%s" % (outfilename,retstr)
@@ -292,3 +297,4 @@ def main(argv=None):
 
 if __name__ == "__main__":
     sys.exit(main())
+    
